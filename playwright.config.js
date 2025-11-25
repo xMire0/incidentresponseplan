8877c1bbfd2c6741bb05d10c17fa1c28011cb19b
Moe241 <113907166+Moe241@users.noreply.github.com>
2025-11-25 21:16:08 +0100

    Diagrams and playwright extension for testing

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: 'html',
+
+  use: {
+    baseURL: 'http://localhost:5173',   // <--- your frontend
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
+});
